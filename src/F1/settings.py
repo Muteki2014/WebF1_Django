4725197fb6e258b8c5d5a016e78b2c8a2bc6ac3a
Matias <salvo253@gmail.com>
2025-12-01 01:59:11 -0300

    Fix: agregar ALLOWED_HOSTS para Render

--- src/F1/settings.py
+++ src/F1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,11 @@ SECRET_KEY = 'django-insecure-i-5_9k0rbr@)e@@f3q6(9(7xe_sism&+r_0%^f#5fk@pfj^4uk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME'),
+]
 
 
 # Application definition
